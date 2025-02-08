249938866a46cd23da501568fd911702c180a2b0
javeria-fatima49 <fatimajaveria409@gmail.com>
2025-02-08 23:55:52 +0500

    admin dashboard push to github

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/ui/Header";
-import SideNavbar from "@/components/ui/Sidenav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
