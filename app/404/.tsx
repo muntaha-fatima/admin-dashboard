f6df50585c37e6b6c2499e1469d231748130c654
muntaha-fatima <shazsabir6@gmail.com>
2025-05-14 04:50:05 -0700

    api push toh github

---
+++ app/404/.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react"
+import Client404 from "../../components/client404"
+
+export default function NotFoundPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Client404 />
+    </Suspense>
+  )
+}
---
+++ components/client404.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+export default function Client404() {
+  const params = useSearchParams()
+  const search = params.get('q')
+
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      {search && <p>You searched for: {search}</p>}
+    </div>
+  )
+}
