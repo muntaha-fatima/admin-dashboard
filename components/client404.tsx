2a1711a66be096b61ad5e5a5bbd58de7eee10e1b
muntaha-fatima <shazsabir6@gmail.com>
2025-05-15 11:16:52 -0700

    api push to github

--- components/client404.tsx
+++ components/client404.tsx
@@ -1,16 +1,16 @@
-// components/client404.tsx
-'use client'
+// // components/client404.tsx
+// 'use client'
 
-import { useSearchParams } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
 
-export default function Client404() {
-  const params = useSearchParams()
-  const search = params.get('q')
+// export default function Client404() {
+//   const params = useSearchParams()
+//   const search = params.get('q')
 
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      {search && <p>You searched for: {search}</p>}
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h1>404 - Page Not Found</h1>
+//       {search && <p>You searched for: {search}</p>}
+//     </div>
+//   )
+// }
