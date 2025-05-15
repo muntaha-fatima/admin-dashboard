ef818581285364cd4688cf8029511411e87fbdbe
muntaha-fatima <shazsabir6@gmail.com>
2025-05-15 11:05:00 -0700

    api push to github

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -1,7 +1,9 @@
+import Client404 from "@/components/client404";
 export default function NotFound() {
   return (
     <div className="min-h-screen flex items-center justify-center text-center">
       <div>
+         <Client404 />
         <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
         <p className="mt-2 text-lg text-muted-foreground">
           Sorry, the page you are looking for does not exist.
