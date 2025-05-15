8abb199c999cef2622832270d54233a1e9dce330
muntaha-fatima <shazsabir6@gmail.com>
2025-05-15 11:08:32 -0700

    api push to github

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -1,14 +1,10 @@
+import { Suspense } from "react";
 import Client404 from "@/components/client404";
+
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-center">
-      <div>
-         <Client404 />
-        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-        <p className="mt-2 text-lg text-muted-foreground">
-          Sorry, the page you are looking for does not exist.
-        </p>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading 404...</div>}>
+      <Client404 />
+    </Suspense>
   );
 }
