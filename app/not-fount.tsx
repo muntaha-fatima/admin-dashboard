b24a5b769ac0e8f209c74a5b1524f4e8f4356f9f
muntaha-fatima <shazsabir6@gmail.com>
2025-05-15 10:59:05 -0700

    api push to github

---
+++ app/not-fount.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+        <p className="mt-2 text-lg text-muted-foreground">
+          Sorry, the page you are looking for does not exist.
+        </p>
+      </div>
+    </div>
+  );
+}
