5734d4c2cd9a6a0d8b4867f2ec3ab3ca9c44f830
muntaha-fatima <shazsabir6@gmail.com>
2025-08-18 17:15:46 +0500

    admin push to github

--- app/api/booklibrary/route.ts
+++ app/api/booklibrary/route.ts
@@ -321,7 +321,7 @@
 
 import { type NextRequest, NextResponse } from "next/server"
 import { connectToDatabase } from "@/lib/mongodb"
-import { Book } from "../../../models/Book"
+import { Book } from "../../../models/book"
 import { Promo } from "../../../models/promoimage" // Assuming this path is correct for your Mongoose Promo model
 
 // ✅ Allowed origins (Prioritizing Admin Dashboard)
