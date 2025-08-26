91e97618f59393425483190b243a75920deaf36e
muntaha-fatima <shazsabir6@gmail.com>
2025-08-26 23:23:26 +0500

    admin push to github

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-=======
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-  },
->>>>>>> ce28dbebab8bc9b8024372057b421aadc8088041
-};
+    "@tailwindcss/postcss": {}
+  }
+}
 
 export default config;
--- src/sanity/lib/order.ts
+++
@@ -1,6 +0,0 @@
-'use client';
-import { client } from "./client";
-
-export async function fetchOrders() {
-  return await client.fetch('*[_type == "orders"]');
-}
