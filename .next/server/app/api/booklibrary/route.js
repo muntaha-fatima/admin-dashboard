fa5980459d577c1ba81c27931a8c9d90e6fbdd84
muntaha-fatima <shazsabir6@gmail.com>
2025-08-27 11:20:33 +0500

    Restore .gitignore and remove node_modules/build from tracking

--- .next/server/app/api/booklibrary/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/booklibrary/route.js")
-R.c("server/chunks/node_modules_next_ee98e23c._.js")
-R.c("server/chunks/[root-of-the-server]__ee3c54a6._.js")
-R.m("[project]/.next-internal/server/app/api/booklibrary/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/booklibrary/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/booklibrary/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/static/chunks/app_admin_page_tsx_b2f3bef0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_b90201b3._.js",
-  "static/chunks/node_modules_19ac386e._.js"
-],
-    source: "dynamic"
-});
